e842f591572948687fcc6490a5e54b052781de47
mariosepulvedae <eduardo.sh3100@gmail.com>
2026-01-11 00:24:06 -0600

    Added tint function

--- sketch.js
+++ sketch.js
@@ -7,9 +7,12 @@ let names=["/images/dvd_cyan.png",
   "/images/dvd_whte.png",
   "/images/dvd_yellow.png"];
 
-let x = 100;let y = 100;
+let x = 500;let y = 100;
 let dx = 2; let dy = 2;
 let item=0;
+let R=255;
+let G=0;
+let B=0;
 
 function preload(){
   for(var i=0;i<names.length;i++){
@@ -18,14 +21,15 @@ function preload(){
 }
 
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(1290, 600);
 }
 
 function draw() {
   background(0);
   rect(x,y,100,50);
   fill(0,0,0);
-  image(dvd[item],x,y,100,50);
+  image(dvd[4],x,y,100,50);
+  tint(R,G,B,255);
   x = x + dx;
   y = y + dy;
   edgeHandling();
@@ -35,10 +39,27 @@ function edgeHandling(){
   if(x <= 0 || x + 100>= width){
     dx = dx*-1;  
     item = Math.floor(Math.random() * 6);
+    changeColor();
   }
 
   if(y+10<=0 || y+50>=height){
     dy = dy*-1;
     item = Math.floor(Math.random() * 6);
+    changeColor();
+  }
+}
+
+function changeColor(){
+  R=Math.floor(Math.random()*255);
+  G=Math.floor(Math.random()*255);
+  B=Math.floor(Math.random()*255);
+  while(R<100){
+    R=Math.floor(Math.random()*255);
+  }
+  while(G<100){
+    G=Math.floor(Math.random()*255);
+  }
+  while(B<100){
+    B=Math.floor(Math.random()*255);
   }
 }
